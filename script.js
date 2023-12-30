7c8382aef7d84a2a45b507ee28bf165eea66f2ae
medvegergo <medvegergo@gszi.edu.hu>
2023-12-30 17:52:38 +0100

    scrolling function to navItems done

---
+++ script.js
@@ -0,0 +1,21 @@
+let firstContentSection = document.getElementById("firstDiv");
+let secondContentSection = document.getElementById("secondDiv");
+let thirdContentSection = document.getElementById("thirdDiv");
+let fourthContentSection = document.getElementById("fourthDiv");
+
+
+document.getElementById("navOpt1").addEventListener("click", function(){
+    firstContentSection.scrollIntoView({ behavior: "smooth" , block: "end"});
+});
+
+document.getElementById("navOpt2").addEventListener("click", function(){
+    secondContentSection.scrollIntoView({ behavior: "smooth" , block: "end"});
+});
+
+document.getElementById("navOpt3").addEventListener("click", function(){
+    thirdContentSection.scrollIntoView({ behavior: "smooth" , block: "end"});
+});
+
+document.getElementById("navOpt4").addEventListener("click", function(){
+    fourthContentSection.scrollIntoView({ behavior: "smooth" , block: "end"});
+});
